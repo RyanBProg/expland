bf92a5b03f977f4f91a1442014f15a86bc78e52c
Ryan <ryanbprog@gmail.com>
2025-06-26 13:07:45 +0100

    added loading state for geojson and also imported typing for travel fetch

--- apps/web-app/src/routes/dashboard/MapView.tsx
+++ apps/web-app/src/routes/dashboard/MapView.tsx
@@ -1,10 +1,11 @@
 import { Map, Source, Layer, type MapRef } from "react-map-gl/maplibre";
 import "maplibre-gl/dist/maplibre-gl.css";
 import { useEffect, useRef, useState, type RefObject } from "react";
-import { Button, Card, Flex, NativeSelect, Field } from "@chakra-ui/react";
+import { Button, Card, Flex, NativeSelect, Field, Spinner } from "@chakra-ui/react";
 import mapStyle from "../../../public/mapStyle.json";
 import { CaretLeft } from "phosphor-react";
 import ProgressBar from "@/components/dashboard/progress/ProgressBar";
+import type { TravelPreview, TravelsPreviewResponse } from "@/utils/types";
 
 const geoJsonUrl =
   "https://raw.githubusercontent.com/datasets/geo-countries/master/data/countries.geojson";
@@ -42,28 +43,9 @@ const boundsByContinent = {
 
 type Continent = keyof typeof boundsByContinent;
 
-type City = {
-  id: number;
-  name: string;
-};
-
-export type Travel = {
-  id: number;
-  description: string;
-  dateTravel: string;
-  duration: number;
-  country: {
-    id: number;
-    name: string;
-    iso_2: string;
-    continents: string[];
-  };
-  cities: City[];
-};
-
 export default function MapView() {
   const [geoData, setGeoData] = useState(null);
-  const [fetchedTravels, setFetchedTravels] = useState<Travel[]>([]);
+  const [fetchedTravels, setFetchedTravels] = useState<TravelPreview[]>([]);
 
   const fetchTravels = async () => {
     try {
@@ -71,7 +53,7 @@ export default function MapView() {
         method: "GET",
         credentials: "include",
       });
-      const { data } = await res.json();
+      const { data } = (await res.json()) as TravelsPreviewResponse;
 
       setFetchedTravels(data);
     } catch (error) {
@@ -93,48 +75,63 @@ export default function MapView() {
     mapRef.current?.getMap().fitBounds(bounds, { padding: 40, duration: 1200 });
   };
 
-  const travelledCodes = new Set(fetchedTravels.map(travel => travel.country.iso_2));
-  const travelledCodesArr = Array.from(travelledCodes);
+  const travelledCodes = new Set(fetchedTravels.map(travel => travel.country?.iso_2));
+  const travelledCodesArr = Array.from(travelledCodes).filter(
+    (code): code is string => typeof code === "string",
+  );
 
   return (
     <>
       <StatsDrawer flyToBounds={flyToBounds} fetchedTravels={fetchedTravels} />
 
-      <Map
-        ref={mapRef}
-        mapLib={import("maplibre-gl")}
-        initialViewState={{ longitude: 0, latitude: 20, zoom: 1 }}
-        style={{ width: "100vw", height: "calc(100vh - 72px)" }}
-        mapStyle={mapStyle}
-        maxZoom={5}
-        minZoom={1}
-      >
-        {geoData && (
+      {geoData ? (
+        <Map
+          ref={mapRef}
+          mapLib={import("maplibre-gl")}
+          initialViewState={{ longitude: 0, latitude: 20, zoom: 1 }}
+          style={{ width: "100vw", height: "calc(100vh - 72px)" }}
+          mapStyle={mapStyle}
+          maxZoom={5}
+          minZoom={1}
+        >
           <Source id="countries" type="geojson" data={geoData}>
-            <Layer
-              id="country-fills"
-              type="fill"
-              paint={{
-                "fill-color": [
-                  "match",
-                  ["get", "ISO3166-1-Alpha-2"],
-                  travelledCodesArr,
-                  "rgb(53, 163, 55)", // Highlighted countries
-                  "rgba(0,0,0,0)",
-                ],
-                "fill-opacity": 0.5,
-              }}
-            />
+            {travelledCodesArr.length > 0 && (
+              <Layer
+                id="country-fills"
+                type="fill"
+                paint={{
+                  "fill-color": [
+                    "match",
+                    ["get", "ISO3166-1-Alpha-2"],
+                    travelledCodesArr,
+                    "rgb(53, 163, 55)", // Highlighted countries
+                    "rgba(0,0,0,0)",
+                  ],
+                  "fill-opacity": 0.5,
+                }}
+              />
+            )}
           </Source>
-        )}
-      </Map>
+        </Map>
+      ) : (
+        <div
+          css={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "calc(100vh - 72px)",
+          }}
+        >
+          <Spinner size="xl" />
+        </div>
+      )}
     </>
   );
 }
 
 type StatsDrawerProps = {
   flyToBounds: (bounds: any) => void;
-  fetchedTravels: Travel[];
+  fetchedTravels: TravelPreview[];
 };
 
 function StatsDrawer({ flyToBounds, fetchedTravels }: StatsDrawerProps) {
@@ -247,7 +244,7 @@ function DropdownCard({ flyToBounds, dropdownRef }: DropdownCardProps) {
 }
 
 type ProgressCardType = {
-  fetchedTravels: Travel[];
+  fetchedTravels: TravelPreview[];
 };
 
 function ProgressCard({ fetchedTravels }: ProgressCardType) {
@@ -257,20 +254,24 @@ function ProgressCard({ fetchedTravels }: ProgressCardType) {
   );
   const prevTravelsArr = Array.from(prevTravels);
 
-  const europeCount = prevTravelsArr.filter(travel => travel.country.continents.includes("Europe"));
+  const europeCount = prevTravelsArr.filter(travel =>
+    travel.country?.continents.includes("Europe"),
+  );
   const northAmericaCount = prevTravelsArr.filter(travel =>
-    travel.country.continents.includes("North America"),
+    travel.country?.continents.includes("North America"),
   );
   const southAmericaCount = prevTravelsArr.filter(travel =>
-    travel.country.continents.includes("South America"),
+    travel.country?.continents.includes("South America"),
   );
-  const asiaCount = prevTravelsArr.filter(travel => travel.country.continents.includes("Asia"));
+  const asiaCount = prevTravelsArr.filter(travel => travel.country?.continents.includes("Asia"));
   const oceaniaCount = prevTravelsArr.filter(travel =>
-    travel.country.continents.includes("Oceania"),
+    travel.country?.continents.includes("Oceania"),
   );
-  const africaCount = prevTravelsArr.filter(travel => travel.country.continents.includes("Africa"));
+  const africaCount = prevTravelsArr.filter(travel =>
+    travel.country?.continents.includes("Africa"),
+  );
   const antarctica = prevTravelsArr.filter(travel =>
-    travel.country.continents.includes("Antarctica"),
+    travel.country?.continents.includes("Antarctica"),
   );
 
   return (
