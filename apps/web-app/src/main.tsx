6b092b91393b29c7a3842de706e2e03e817c4074
Ryan <ryanbprog@gmail.com>
2025-06-29 20:20:55 +0100

    react router added to project

--- apps/web-app/src/main.tsx
+++ apps/web-app/src/main.tsx
@@ -16,6 +16,9 @@ import Policies from "./routes/Policies";
 import MyTravels from "./routes/dashboard/MyTravels";
 import { Toaster } from "./components/ui/toaster";
 import MapView from "./routes/dashboard/MapView";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 const router = createBrowserRouter([
   {
@@ -47,9 +50,11 @@ const router = createBrowserRouter([
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Provider>
-      <RouterProvider router={router} />
-      <Toaster />
-    </Provider>
+    <QueryClientProvider client={queryClient}>
+      <Provider>
+        <RouterProvider router={router} />
+        <Toaster />
+      </Provider>
+    </QueryClientProvider>
   </StrictMode>,
 );
